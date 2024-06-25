0d39449e8ceb0f69550343e52e12bde631663b77
Mathew Harb <30761383+mathewharb@users.noreply.github.com>
2024-06-25 14:21:02 +0000

    Add files via upload

---
+++ BookingTable.Web/obj/Release/Package/PackageTmp/Scripts/common.js
@@ -0,0 +1,34 @@
+﻿function ShowPopup(data) {
+    $("#popupContainer").html(data);
+    $("#popupModal").modal("show");
+}
+function ClosePopup() {
+    $("#popupContainer").html("");
+    $("#popupModal").modal("hide");
+}
+function ShowNotification(data) {
+
+    $("#messageTitle").text(data.Title);
+    $("#messageContent").text(data.Content);
+
+    if (data.Type === "success") {
+        ClosePopup();
+        $("#messageModal").addClass("modal-primary");
+    } else {
+        $("#messageModal").addClass("modal-danger");
+    }
+
+    $("#messageModal").modal("show");
+
+    if (data.Type === "success") {
+        setTimeout(function() {
+                history.go(0);
+            },
+            1000);
+    } else {
+        setTimeout(function () {
+                $("#messageModal").modal("hide");
+        },
+            1500);
+    }
+}
